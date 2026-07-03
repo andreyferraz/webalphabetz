4e14c2f9e7610e9cf8038a3c409774ee3dc7b9bd
andreyferraz <andrey.developer@hotmail.com>
2026-07-03 10:25:33 -0300

    first commit

---
+++ src/main/java/com/alphabetz/webalphabetz/WebalphabetzApplication.java
@@ -0,0 +1,13 @@
+package com.alphabetz.webalphabetz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebalphabetzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebalphabetzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alphabetz/webalphabetz/WebalphabetzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alphabetz.webalphabetz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebalphabetzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
