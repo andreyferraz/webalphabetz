2601efeaf036e311cc156f255ea4c7085952fe11
andreyferraz <andrey.developer@hotmail.com>
2026-08-11 11:26:28 -0300

    feat: add TurmasImagens model and repository for managing images associated with classes

---
+++ src/main/java/com/alphabetz/webalphabetz/model/TurmasImagens.java
@@ -0,0 +1,44 @@
+package com.alphabetz.webalphabetz.model;
+
+import java.util.UUID;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.domain.Persistable;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table("turmas_imagens")
+public class TurmasImagens implements Persistable<UUID> {
+
+    @Id
+    @Column("id")
+    private UUID id;
+
+    @Column("nome")
+    private String nome;
+
+    @Column("imagem_url")
+    private String imagemUrl;
+
+    @Transient
+    private boolean isNew;
+
+    @Override
+    public UUID getId() {
+        return id;
+    }
+
+    @Override
+    public boolean isNew() {
+        return isNew;
+    }
+
+}
