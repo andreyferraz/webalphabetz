4aeecf447dc9928de6f44b9dbfd863cd9365a841
andreyferraz <andrey.developer@hotmail.com>
2026-08-11 15:44:09 -0300

    fix: remove unused imports for improved code clarity and maintainability

--- src/test/java/com/alphabetz/webalphabetz/WebalphabetzApplicationTests.java
+++ src/test/java/com/alphabetz/webalphabetz/WebalphabetzApplicationTests.java
@@ -1,13 +1,7 @@
 package com.alphabetz.webalphabetz;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-
 import org.junit.jupiter.api.Test;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class WebalphabetzApplicationTests {
