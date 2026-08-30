883ca3fdb04d6c6a42385a97c64c85e92b5f9440
andreyferraz <andrey.developer@hotmail.com>
2026-08-30 12:30:42 -0300

    fix: update Java version to 21 in pom.xml and run.sh for compatibility

--- run.sh
+++ run.sh
@@ -1,25 +1,9 @@
 #!/bin/bash
-export JAVA_HOME="/Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Home"
+export JAVA_HOME="$(/usr/libexec/java_home -v 21)"
 export PATH="$JAVA_HOME/bin:$PATH"
 
 echo "Usando Java: $(java -version 2>&1 | head -1)"
-
-if [ -x ./mvnw ]; then
-  exec ./mvnw spring-boot:run "$@"
-else
-  exec mvn spring-boot:run "$@"
-fi
-#!/bin/bash
-export JAVA_HOME="/Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Home"
-export PATH="$JAVA_HOME/bin:$PATH"
-
-echo "Usando Java: $(java -version 2>&1 | head -1)"
-
-if [ -x ./mvnw ]; then
-	exec ./mvnw spring-boot:run "$@"
-else
-	exec mvn spring-boot:run "$@"
-fi
+./mvnw spring-boot:run "$@"
 
 ##comando para dar permissão de execução no arquivo run.sh
 ##chmod +x run.sh
